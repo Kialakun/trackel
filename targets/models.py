262898e28c132a6723cd24b56b4d9063fad2fdda
Kialakun <kialakunngalgal@gmail.com>
2020-09-19 16:48:40 +1000

    Update models.py

--- targets/models.py
+++ targets/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class target(models.Model):
+    """Django data model target"""
+    name = models.CharField(blank=True, max_length=100)
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'target'
+        verbose_name_plural = 'targets'
+
+    def __str__(self):
+        return str(self.id)
