25b79cfa04ac3171f8d771f64c51721aa679f355
Kialakun <kialakunngalgal@gmail.com>
2020-09-19 20:31:59 +1000

    Update elCalculations.js

--- static/js/elCalculations.js
+++ static/js/elCalculations.js
@@ -1,5 +1,26 @@
 function calculateOtherLosses (extractLoss, heuft1Loss, heuft2Loss) {
+  // calculate losses for the Loss Summary
   var otherLoss = extractLoss - heuft1Loss - heuft2Loss;
-  console.log(extractLoss);
   return otherLoss;
+}
+
+function conditionalColoring (chart, target) {
+  // conditional colring for charts
+  var red = 'rgb(255, 0, 0, 0.5)';
+  var redBorder = 'rgb(255, 0, 0, 1)';
+
+  console.log(chart.config.data.datasets);
+
+  var datasets = chart.config.data.datasets;
+
+  for (var k in datasets) {
+    for (var i in datasets[k].data) {
+      if (datasets[k].data[i] > target) {
+        // change color of background and border to red
+        datasets[k].backgroundColor[i] = red;
+        datasets[k].borderColor[i] = redBorder;
+      }
+    }
+  }
+  chart.update();
 }
